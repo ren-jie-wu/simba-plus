380e6c471748e0395a69bf3d14391efe2ab602c3
Renjie Wu <renjiewu@mit.edu>
2025-11-25 13:12:12 -0500

    quick syntax fix

--- src/simba_plus/train.py
+++ src/simba_plus/train.py
@@ -338,7 +338,7 @@ def human_format(num):
     while abs(num) >= 1000:
         magnitude += 1
         num /= 1000.0
-    return f"{str(num).rstrip('0').rstrip('.')}{["", "K", "M", "B", "T"][magnitude]}"
+    return f"{str(num).rstrip('0').rstrip('.')}{['', 'K', 'M', 'B', 'T'][magnitude]}"
 
 
 def save_files(run_id, cell_adata=None, peak_adata=None):
